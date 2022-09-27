006453f63425a9104047d4894589ac51bc96e0f1
Wale <wtoalabi@gmail.com>
2022-09-27 18:25:03 +0100

    init

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 pip install git+https://github.com/openai/whisper.git
 sudo apt update && sudo apt install  --yes
-whisper sample1.mp3 --model large  --language English
+sudo wget https://github.com/wtoalabi/whisper/raw/main/sample_1.mp3
+sudo wget https://github.com/wtoalabi/whisper/raw/main/sample1.mp4
+sudo whisper sample_1.mp3 --model large  --language English
