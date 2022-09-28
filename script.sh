24985744215115a53a34e9dc1f59f03ca30922fc
Wale <wtoalabi@gmail.com>
2022-09-28 12:12:22 +0100

    init

--- script.sh
+++ script.sh
@@ -2,5 +2,6 @@
 pip install git+https://github.com/openai/whisper.git
 sudo apt update && sudo apt install ffmpeg  --yes
 sudo wget https://github.com/wtoalabi/whisper/raw/main/sample_1.mp3
+sudo wget https://github.com/wtoalabi/whisper/raw/main/rap.mp3
 sudo wget https://github.com/wtoalabi/whisper/raw/main/sample1.mp4
 sudo whisper sample_1.mp3 --model tiny  --language English
